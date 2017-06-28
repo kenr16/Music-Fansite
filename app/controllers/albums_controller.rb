c1f088b4a7eac06ac1886c375df8836631831b87
Ken Rutan and Sowmya Dinavahi <sowmya@>
2017-06-28 09:13:32 -0700

    add crud functionality for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -24,9 +24,29 @@ class AlbumsController < ApplicationController
     end
   end
 
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @album= Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to albums_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to albums_path
+  end
+
   private
   def album_params
     params.require(:album).permit(:name, :artist, :genre, :image)
   end
-  
+
 end
